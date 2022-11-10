c329705a6a117d788b93b39a6cea87c4f3104c89
Ximeng Zhang <xmg.zhang@gmail.com>
2022-11-10 12:01:25 -0300

    Completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, finish) {
+    if (!Number.isInteger(start) || !Number.isInteger(finish)) return 'ERROR';
+    if (start < 0 || finish < 0) return 'ERROR';
 
+    let result = 0;
+    while (start !== finish) {
+        result += start;
+        start < finish ? start++ : start--;
+    }
+    result += finish;
+    return result;
 };
 
 // Do not edit below this line

5557fd2c46d98238f03c4e9ef8b15c3b833920c1
Ximeng Zhang <xmg.zhang@gmail.com>
2022-11-10 11:38:26 -0300

    Completed removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray) {
+    if (arguments.length <= 1) { return originalArray }
 
+    const argArray = argToArray(arguments);
+    const itemsToBeFiltered = argArray.slice(1, argArray.length);
+
+    for (let item of itemsToBeFiltered) {
+        originalArray = originalArray.filter(value => value !== item);
+    }
+    return originalArray
 };
 
+function argToArray(argObject) {
+    const result = [];
+    for (let i = 0; i < argObject.length; i++) {
+        result.push(argObject[i]);
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = removeFromArray;

afee13fe01c2a878dd41f51c8f02e3db5230a2c5
Ximeng Zhang <xmg.zhang@gmail.com>
2022-11-10 10:16:51 -0300

    Completed reverseString.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let resultArray = [];
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        resultArray.push(stringArray.pop());
+    }
+    return resultArray.join('');
 };
 
 // Do not edit below this line

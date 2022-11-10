ff46f8316fd7538df4b23fda2d27cda6500ac974
Ximeng Zhang <xmg.zhang@gmail.com>
2022-11-10 10:09:08 -0300

    Completed repeatStrings.js.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTimes) {
+    if (repeatTimes < 0) {return 'ERROR'}
+    let result = '';
+    while (repeatTimes > 0) {
+        result += string;
+        repeatTimes--;
+    }
+    return result;
 };
 
 // Do not edit below this line
